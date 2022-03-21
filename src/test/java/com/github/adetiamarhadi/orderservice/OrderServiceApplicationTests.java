341aeac3c4c2351af06a26e094e6f6d48b725e4e
Adet <adetia.marhadi>
2022-03-21 16:26:24 +0700

    initial commit

---
+++ src/test/java/com/github/adetiamarhadi/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.adetiamarhadi.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
