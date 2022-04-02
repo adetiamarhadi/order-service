d371430aa88a91e45561b78c8ce5e9833cae5491
adetiamarhadi <adetia.marhadi@gmail.com>
2022-04-02 17:24:14 +0700

    send query to user service

--- src/main/java/com/github/adetiamarhadi/orderservice/saga/OrderSaga.java
+++ src/main/java/com/github/adetiamarhadi/orderservice/saga/OrderSaga.java
@@ -3,9 +3,13 @@ package com.github.adetiamarhadi.orderservice.saga;
 import com.github.adetiamarhadi.orderservice.core.events.OrderCreatedEvent;
 import com.github.adetiamarhadi.sagacore.commands.ReserveProductCommand;
 import com.github.adetiamarhadi.sagacore.events.ProductReservedEvent;
+import com.github.adetiamarhadi.sagacore.model.User;
+import com.github.adetiamarhadi.sagacore.query.FetchUserPaymentDetailsQuery;
 import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.axonframework.messaging.responsetypes.ResponseTypes;
 import org.axonframework.modelling.saga.SagaEventHandler;
 import org.axonframework.modelling.saga.StartSaga;
+import org.axonframework.queryhandling.QueryGateway;
 import org.axonframework.spring.stereotype.Saga;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,10 +21,16 @@ public class OrderSaga {
 	private static final Logger LOGGER = LoggerFactory.getLogger(OrderSaga.class);
 
 	private transient CommandGateway commandGateway;
+	private transient QueryGateway queryGateway;
 
 	@Autowired
 	public void setCommandGateway(CommandGateway commandGateway) {
 		this.commandGateway = commandGateway;
+	}
+
+	@Autowired
+	public void setQueryGateway(QueryGateway queryGateway) {
+		this.queryGateway = queryGateway;
 	}
 
 	@StartSaga
@@ -46,5 +56,25 @@ public class OrderSaga {
 	@SagaEventHandler(associationProperty = "orderId")
 	public void handle(ProductReservedEvent productReservedEvent) {
 		LOGGER.info("ProductReservedEvent is called for productId: " + productReservedEvent.getProductId() + " and orderId: " + productReservedEvent.getOrderId());
+
+		FetchUserPaymentDetailsQuery query = FetchUserPaymentDetailsQuery.builder()
+				.userId(productReservedEvent.getUserId())
+				.build();
+
+		User user = null;
+
+		try {
+			user = queryGateway.query(query, ResponseTypes.instanceOf(User.class)).join();
+		} catch (Exception e) {
+			LOGGER.error(e.getMessage(), e);
+			return;
+		}
+
+		if (null == user) {
+			LOGGER.warn("user id not found.");
+			return;
+		}
+
+		LOGGER.info("successfully fetched user payment details for user " + user.getFirstName());
 	}
 }
